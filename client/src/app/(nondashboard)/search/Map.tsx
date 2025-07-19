16c2b1b098dcaa1c6dcb6cc7824ea6544b484a67
Jangchaeyun <zzangsally@naver.com>
2025-07-19 23:20:08 +0900

    0719 Map

--- client/src/app/(nondashboard)/search/Map.tsx
+++ client/src/app/(nondashboard)/search/Map.tsx
@@ -1,18 +1,51 @@
-import React, { useRef } from "react";
+"use client";
+import React, { useEffect, useRef } from "react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useAppSelector } from "@/state/redux";
+import { useGetPropertiesQuery } from "@/state/api";
 
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN as string;
 
 const Map = () => {
   const mapContainerRef = useRef(null);
   const filters = useAppSelector((state) => state.global.filters);
-  const isFiltersFullOpen = useAppSelector(
-    (state) => state.global.isFiltersFullOpen
-  );
 
-  return <div>Map</div>;
+  const {
+    data: properties,
+    isLoading,
+    isError,
+  } = useGetPropertiesQuery(filters);
+  console.log("properties:", properties);
+
+  useEffect(() => {
+    if (isLoading || isError || !properties) return;
+
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current!,
+      style: "mapbox://styles/cherry1112/cmdabvjmi02tk01ri5388ev37",
+      center: filters.coordinates || [-74.5, 40],
+      zoom: 9,
+    });
+
+    const resizeMap = () => setTimeout(() => map.resize(), 700);
+    resizeMap();
+
+    return () => map.remove();
+  }, [properties, isLoading, isError, filters.coordinates]);
+
+  return (
+    <div className="basis-5/12 grow relative rounded-xl">
+      <div
+        className="map-container rounded-xl"
+        ref={mapContainerRef}
+        style={{
+          height: "100%",
+          width: "100%",
+        }}
+      />
+    </div>
+  );
 };
 
 export default Map;
